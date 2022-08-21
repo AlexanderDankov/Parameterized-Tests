d4a20eb113e7e7bab2ef66f8385b6e9411c299ac
AleksandrDankov <dankoffalexander@gmail.com>
2022-08-21 18:50:10 +0500

    Update remote url to local selenoid

--- src/test/java/com/simbirsoft/tests/TestBase.java
+++ src/test/java/com/simbirsoft/tests/TestBase.java
@@ -2,19 +2,16 @@ package com.simbirsoft.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import com.simbirsoft.config.CredentialConfig;
+
 import com.simbirsoft.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.aeonbits.owner.ConfigFactory;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import static java.lang.String.format;
 
 public class TestBase {
-
-    public static CredentialConfig credentials = ConfigFactory.create(CredentialConfig.class);
 
     @BeforeAll
     static void setup() {
@@ -22,8 +19,7 @@ public class TestBase {
         Configuration.startMaximized = true;
         SelenideLogger.addListener("AllureListener", new AllureSelenide());
 
-        String remoteUrl = "selenoid.autotests.cloud";
-        Configuration.remote = format("https://%s:%s@%s/wd/hub/", credentials.login(), credentials.password(), remoteUrl);
+        Configuration.remote = "http://localhost:8080/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
